704ee6dad7d7b4d526794511ba2022b02926f1af
Matthew Briley <mcbriley@Matthews-MacBook-Pro-2.local>
2018-04-28 10:08:20 -0400

    done

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES("Breakfast Burger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES("Bourbon Burger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES("Bacon Burger", false);
